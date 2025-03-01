7ef93fb31a210b50c54fd8428b1963d8fde988c6
Zidan102 <yogumetokisaki@gmail.com>
2025-03-01 19:02:57 +0700

    mengubah beberapa if dengan elif

--- Sesi 3/Tugas 3/Check.py
+++ Sesi 3/Tugas 3/Check.py
@@ -6,13 +6,13 @@ genap = (a % 2) == 0
 ganjil = (c % 2) != 0
 if a > b and b > c and b != c and ganjil is True and genap is True:
     print('Angka Valid')
-if ganjil is False:
+elif ganjil is False:
     print('Angka tidak valid karna c bukan ganjil')
-if genap is False:
+elif genap is False:
     print('Angka tidak valid karna a bukan genap')
-if a < b:
+elif a < b:
     print('Angka tidak valid karna a lebih kecil daripada b')
-if b < c:
+elif b < c:
     print('Angka tidak valid karna b lebih kecil daripada c')
-if b == c:
+elif b == c:
     print('Angka tidak valid karna b dan c nilainya sama')
